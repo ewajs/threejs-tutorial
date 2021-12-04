0b368ee175c66df078771a6edb65c263184f27af
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2021-12-04 16:49:10 -0300

    Rotating moon with earth axial tilt and moon displacement

--- src/script.js
+++ src/script.js
@@ -1,19 +1,33 @@
 import "./style.css";
 import * as THREE from "three";
+import { MathUtils } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
 
-// Constants
+/// Constants
 //// Accurate Astronomical Constants (km)
 const EARTH_RADIUS = 6357;
 const MOON_RADIUS = 1737;
 const SCALE = 1 / EARTH_RADIUS;
+const EARTH_AXIAL_TILT = (23.4 * Math.PI) / 180; // tilt in radians
 
-//// Fake Astronomical Constants
+//// Fake Astronomical Constants (seconds, radians)
 const EARTH_MOON_DISTANCE = 2.5 * EARTH_RADIUS;
-const MOON_ROTATIONAL_PERIOD = 10; // In seconds
+const EARTH_SUN_DISTANCE = 12 * EARTH_MOON_DISTANCE;
+const SUN_RADIUS = 10000;
+const ATMOSPHERE_RADIUS = EARTH_RADIUS + 100;
+const SUN_ROTATIONAL_PERIOD = 30;
+const MOON_ROTATIONAL_PERIOD = 10;
+// We'll make time scales accurate relative to moon period
+const EARTH_ROTATIONAL_PERIOD = (1 * MOON_ROTATIONAL_PERIOD) / 28;
 const MOON_ORBITAL_PERIOD = MOON_ROTATIONAL_PERIOD; // At least we'll have tidal locking
 
+// This is fake, we'll make the atmosphere rotate 20% faster than the Earth itself
+const EARTH_ATMOSPHERE_ROTATIONAL_PERIOD = 0.8 * EARTH_ROTATIONAL_PERIOD;
+
+const MOON_ORBITAL_PLANE_INCLINATION = MathUtils.degToRad(5);
+const MOON_ORBITAL_DISPLACEMENT =
+  Math.tan(MOON_ORBITAL_PLANE_INCLINATION) * EARTH_MOON_DISTANCE;
 //// Rendering Constants
 const SPHERE_SEGMENTS = 64;
 
@@ -67,6 +81,14 @@ moonMaterial.map = moonTexture;
 const earth = new THREE.Mesh(earthGeometry, earthMaterial);
 earth.castShadow = true;
 earth.receiveShadow = true;
+earth.geometry.applyMatrix(
+  new THREE.Matrix4().makeRotationZ(-EARTH_AXIAL_TILT)
+);
+const earthAxis = new THREE.Vector3(
+  Math.sin(EARTH_AXIAL_TILT),
+  Math.cos(EARTH_AXIAL_TILT),
+  0
+).normalize();
 scene.add(earth);
 
 //// Moon
@@ -78,17 +100,15 @@ scene.add(moon);
 
 // Lights
 
-const pointLight = new THREE.DirectionalLight(0xffffff, 1);
-pointLight.position.x = 10;
-pointLight.position.y = 0;
-pointLight.position.z = 0;
-pointLight.castShadow = true;
-pointLight.shadow.radius = 50;
-pointLight.shadow.mapSize.width = 4096;
-pointLight.shadow.mapSize.height = 4096;
-scene.add(pointLight);
-
-gui.add(pointLight.position, "y");
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.x = 10;
+directionalLight.position.y = 0;
+directionalLight.position.z = 0;
+directionalLight.castShadow = true;
+directionalLight.shadow.radius = 50;
+directionalLight.shadow.mapSize.width = 4096;
+directionalLight.shadow.mapSize.height = 4096;
+scene.add(directionalLight);
 
 /**
  * Sizes
@@ -147,14 +167,42 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
  */
 
 const clock = new THREE.Clock();
+let lastEarthAngle = 0;
+let lastEarthAtmosphereAngle = 0;
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
-  // Update objects
-  earth.rotation.y = 0.5 * elapsedTime;
-  moon.rotation.y = 0.5 * elapsedTime;
-  //   moon.position.set();
+  const earthAngleDelta =
+    ((((2 * Math.PI) / EARTH_ROTATIONAL_PERIOD) * elapsedTime) %
+      (2 * Math.PI)) -
+    lastEarthAngle;
+  lastEarthAngle += earthAngleDelta;
+
+  const earthAtmosphereAngleDelta =
+    ((((2 * Math.PI) / EARTH_ATMOSPHERE_ROTATIONAL_PERIOD) * elapsedTime) %
+      (2 * Math.PI)) -
+    lastEarthAtmosphereAngle;
+  lastEarthAtmosphereAngle += earthAtmosphereAngleDelta;
+
+  //Update objects
+  earth.rotateOnAxis(earthAxis, earthAngleDelta);
+  moon.rotation.y = ((2 * Math.PI) / MOON_ROTATIONAL_PERIOD) * elapsedTime;
+
+  const newMoonX =
+    EARTH_MOON_DISTANCE *
+    SCALE *
+    Math.cos(-((2 * Math.PI) / MOON_ORBITAL_PERIOD) * elapsedTime);
+  const newMoonY =
+    MOON_ORBITAL_DISPLACEMENT *
+    SCALE *
+    Math.sin(-((2 * Math.PI) / MOON_ORBITAL_PERIOD) * elapsedTime);
+  const newMoonZ =
+    EARTH_MOON_DISTANCE *
+    SCALE *
+    Math.sin(-((2 * Math.PI) / MOON_ORBITAL_PERIOD) * elapsedTime);
+
+  moon.position.set(newMoonX, newMoonY, newMoonZ);
 
   // Update Orbital Controls
   controls.update();
